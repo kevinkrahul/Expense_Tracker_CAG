9fcfed286c73224fea3b93c545f4a9aa4a5b8bb6
kevin <kevinrahul.22ec@kct.ac.in>
2025-06-29 15:45:27 +0530

    Refresh token added

--- Expense_backend/app.py
+++ Expense_backend/app.py
@@ -3,12 +3,13 @@ from flask_cors import CORS
 from Model.Model_Generator import generate_sql_from_query, generate_interactive_response, contextual_query_response, classify_input_type, extract_info
 from Model.SQL_operations import store_transaction, execute_sql_query, inject_user_filter
 from flask_jwt_extended import (
-    JWTManager, create_access_token,
+    JWTManager, create_access_token, create_refresh_token,
     jwt_required, get_jwt_identity
 )
 import bcrypt
 import psycopg2
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 app = Flask(__name__)
@@ -23,6 +24,9 @@ CORS(app,
 
 load_dotenv()
 app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=15)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
+
 jwt = JWTManager(app)
 
 
@@ -54,7 +58,8 @@ def signup():
         user_id = cursor.fetchone()[0]
         conn.commit()
         token = create_access_token(identity=user_id)
-        return jsonify({"token": token, "success": "User registered successfully!"}), 201
+        refresh_token = create_refresh_token(identity=user_id)
+        return jsonify({"token": token,"refresh_token":refresh_token ,"success": "User registered successfully!"}), 201
         
     except Exception as e:
         conn.rollback()
@@ -74,8 +79,19 @@ def login():
 
     if user and bcrypt.checkpw(password.encode(), user[1].encode()):
         token = create_access_token(identity=str(user[0]))
-        return jsonify({"token": token})
+        refresh_token=create_refresh_token(identity=str(user[0]))
+        return jsonify({"token": token,"refresh_token":refresh_token})
     return jsonify({"error": "Invalid credentials"}), 401
+
+
+
+@app.route("/refresh", methods=["POST"])
+@jwt_required(refresh=True)
+def refresh():
+    identity = get_jwt_identity()
+    new_access_token = create_access_token(identity=identity)
+    return jsonify({"access_token": new_access_token})
+
 
 
 
